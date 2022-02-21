2c6949b822ef68563f455cf741edb6edd5f3eef0
Ciobanica Tadeus <ciobanica.tadeus28@gmail.com>
2022-02-21 12:43:38 +0200

    monomial class

---
+++ src/main/java/Monomial.java
@@ -0,0 +1,20 @@
+public class Monomial {
+    private int grad;
+    private int coef;
+
+    public int getGrad() {
+        return grad;
+    }
+
+    public void setGrad(int grad) {
+        this.grad = grad;
+    }
+
+    public int getCoef() {
+        return coef;
+    }
+
+    public void setCoef(int coef) {
+        this.coef = coef;
+    }
+}
