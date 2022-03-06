144956c611941804d3ba4282251ca536245aa86e
Ciobanica Tadeus <ciobanica.tadeus28@gmail.com>
2022-03-06 23:08:41 +0200

    last commit

--- src/main/java/PolynomialCalculator/Calculator.java
+++ src/main/java/PolynomialCalculator/Calculator.java
@@ -10,20 +10,5 @@ public class Calculator {
         Model model = new Model();
         Controller controller = new Controller(model, view);
         view.setVisible(true);
-
-
-
-        /*String input1 = "ce fac?";
-        String input2 = "x-2";
-        controller.checkIfIsCorrect(input2);
-        *//*model.setFirstPolynomial(controller.doPolynomial(input1));
-        model.setSecondPolynomial(controller.doPolynomial(input2));
-        model.setResultPolynomial(model.getSecondPolynomial().multiplyPolynoms(model.getFirstPolynomial(), model.getSecondPolynomial()));
-        model.getResultPolynomial().printPolynom(model.getResultPolynomial());*//*
-        //System.out.println("Rezultatul: " + controller.doString(result));*/
-//        Polynomial polynomial = controller.doPolynomial("x^3+4x^2+5");
-//        polynomial.integratePolynom();
-//        String result = controller.doString(polynomial);
-//        System.out.println(result);
     }
 }
